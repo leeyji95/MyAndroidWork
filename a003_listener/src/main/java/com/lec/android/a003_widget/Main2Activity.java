ad3872c0c3d1ea59904faa45487213c6bafa83fe
leeyji95 <leeyji95@naver.com>
2020-04-15 18:42:18 +0900

    학원에서

--- a003_listener/src/main/java/com/lec/android/a003_widget/Main2Activity.java
+++ a003_listener/src/main/java/com/lec/android/a003_widget/Main2Activity.java
@@ -13,10 +13,10 @@ import android.widget.Toast;
 public class Main2Activity extends AppCompatActivity {
 
     TextView textView; // '계산기'  문자열
-    EditText edit; // number 타입 숫자 입력..
+    EditText edit; // 첫번째 숫자 입력..
     String number; //  내가 입력한 숫자 저장. 즉 숫자->연산자->숫자 에서 첫번째 숫자 저장할 변수
 
-    Button btn_division, btn_multiple, btn_plus, btn_sub, equal;
+    Button btn_division, btn_multi, btn_plus, btn_sub, btn_result;
 
 
     // 과제: 계산기 앱 만들기
@@ -30,28 +30,69 @@ public class Main2Activity extends AppCompatActivity {
         edit = findViewById(R.id.edit);
 
         // 버튼 객체 가져오기
-        btn_division = findViewById(R.id.bttn_division);
-        btn_multiple = findViewById(R.id.bttn_multiple);
-        btn_plus = findViewById(R.id.bttn_plus);
-        btn_sub = findViewById(R.id.bttn_sub);
-        equal = findViewById(R.id.bttn_equal);
+        btn_division = findViewById(R.id.btn_division);
+        btn_multi = findViewById(R.id.btn_multi);
+        btn_plus = findViewById(R.id.btn_plus);
+        btn_sub = findViewById(R.id.btn_sub);
+        btn_result = findViewById(R.id.btn_result);
 
         btn_division.setOnClickListener(myListener);
-        btn_multiple.setOnClickListener(myListener);
+        btn_multi.setOnClickListener(myListener);
         btn_plus.setOnClickListener(myListener);
         btn_sub.setOnClickListener(myListener);
-        equal.setOnClickListener(myListener);
+        btn_result.setOnClickListener(myListener);
 
-        Button clear = findViewById(R.id.bttn_Clear);
+        Button clear = findViewById(R.id.btn_Clear);
         clear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) { // C 버튼 클릭 시
                 number = "";    // number 에는 빈 문자 저장되고,
-                edit.setText("0");    // EditText 엘리먼트에 있는 Text 를 0 문자로 화면에 출력. ->
+                edit.setText("");    // EditText 엘리먼트에 있는 Text 를 0 문자로 화면에 출력. ->
             }
         });
 
-    }  // end onCreate()
+//        Button btn_0 = findViewById(R.id.btn_0);
+//    Button btn_1 = findViewById(R.id.btn_1);
+//    Button btn_2 = findViewById(R.id.btn_2);
+//    Button btn_3 = findViewById(R.id.btn_3);
+//    Button btn_4 = findViewById(R.id.btn_4);
+//    Button btn_5 = findViewById(R.id.btn_5);
+//    Button btn_6 = findViewById(R.id.btn_6);
+//    Button btn_7 = findViewById(R.id.btn_7);
+//    Button btn_8 = findViewById(R.id.btn_8);
+//    Button btn_9 = findViewById(R.id.btn_9);
+//
+//    class MyListener implements View.OnClickListener{ // inner class
+//
+//        String number; // 매개변수 받아줌
+//        public MyListener(String name){this.number=number;}
+//
+//
+//        @Override
+//        public void onClick(View v) {
+//            edit.setText(edit.getText().append(number));  // 기존의 텍스트에 name 을 추가함. 버튼 누를때 마다 그 버튼의 name 을 추가할 것 임.
+//        }
+//    } // end class
+//
+//    // 동일한 동작을 하는 버튼 메소드는 하나의 클래스로 정의한 다음에 쓰는 것이 낫지.
+//        btn_0.setOnClickListener(new MyListener("0"));
+//        btn_1.setOnClickListener(new MyListener("1"));
+//        btn_2.setOnClickListener(new MyListener("2"));
+//        btn_3.setOnClickListener(new MyListener("3"));
+//        btn_4.setOnClickListener(new MyListener("4"));
+//        btn_5.setOnClickListener(new MyListener("5"));
+//        btn_6.setOnClickListener(new MyListener("6"));
+//        btn_7.setOnClickListener(new MyListener("7"));
+//        btn_8.setOnClickListener(new MyListener("8"));
+//        btn_9.setOnClickListener(new MyListener("9"));
+
+}  // end onCreate()
+
+
+
+
+
+
 
     // 연산자에 대한 익명 내부 클래스
     Button.OnClickListener myListener = new Button.OnClickListener(){
@@ -67,42 +108,42 @@ public class Main2Activity extends AppCompatActivity {
         public void onClick(View v) {
 
             switch(v.getId()) {
-                case(R.id.bttn_division) :
+                case(R.id.btn_division) :
                     number = edit.getText().toString(); // 첫번째 숫자 저장
                     edit.setText(""); // 비워주고
                     value = DIVISION;
                     edit.setText(btn_division.getText().toString()); // 나눗셈 텍스트 뽑음..
                     break;
-                case (R.id.bttn_multiple):
+                case (R.id.btn_multi):
                     number = edit.getText().toString();
                     edit.setText("");
                     value = MULTIPLE;
-                    edit.setText(btn_multiple.getText().toString());
+                    edit.setText(btn_multi.getText().toString());
                     break;
-                case(R.id.bttn_plus):
+                case(R.id.btn_plus):
                     number = edit.getText().toString();
                     edit.setText("");
                     value = PLUS;
-                    edit.setText(btn_multiple.getText().toString());
+                    edit.setText(btn_multi.getText().toString());
                     break;
-                case (R.id.bttn_sub):
+                case (R.id.btn_sub):
                     number = edit.getText().toString();
                     edit.setText("");
                     value = SUB;
                     edit.setText(btn_sub.getText().toString());
                     break;
-                case(R.id.bttn_equal):
+                case(R.id.btn_result):
                     if(value == DIVISION){
-                        edit.setText(Integer.parseInt(number) / Integer.parseInt(edit.getText().toString()));
+                        edit.setText("" + (Integer.parseInt(number) / Integer.parseInt(edit.getText().toString())));
                     } else if(value == MULTIPLE) {
-                        edit.setText(Integer.parseInt(number) * Integer.parseInt(edit.getText().toString()));
+                        edit.setText("" + (Integer.parseInt(number) * Integer.parseInt(edit.getText().toString())));
                     } else if(value == PLUS){
-                        edit.setText(Integer.parseInt(number) + Integer.parseInt(edit.getText().toString()));
+                        edit.setText("" + (Integer.parseInt(number) + Integer.parseInt(edit.getText().toString())));
                     } else if(value == SUB){
-                        edit.setText(Integer.parseInt(number) - Integer.parseInt(edit.getText().toString()));
+                        edit.setText("" + (Integer.parseInt(number) - Integer.parseInt(edit.getText().toString())));
                     }
 
-                    number = edit.getText().toString();  // 마지막에 클릭한 숫자가 들어가도록 덮어씌운다. -> 연속 연산 가능
+                    edit.getText();  // 마지막에 클릭한 숫자가 들어가도록 덮어씌운다. -> 연속 연산 가능
                     break;
 
             } // end switch
@@ -111,25 +152,25 @@ public class Main2Activity extends AppCompatActivity {
     }; // end class myListener
 
     public void onClick(View v) {
-
+        int number;
         switch (v.getId()){
-            case R.id.bttn_0: edit.setText(edit.getText().toString()); break;
-            case R.id.bttn_1: edit.setText(edit.getText().toString()); break;
-            case R.id.bttn_2 : edit.setText(edit.getText().toString()); break;
-            case R.id.bttn_3 : edit.setText(edit.getText().toString()); break;
-            case R.id.bttn_4 : edit.setText(edit.getText().toString()); break;
-            case R.id.bttn_5 : edit.setText(edit.getText().toString()); break;
-            case R.id.bttn_6 : edit.setText(edit.getText().toString()); break;
-            case R.id.bttn_7 : edit.setText(edit.getText().toString()); break;
-            case R.id.bttn_8 : edit.setText(edit.getText().toString()); break;
-            case R.id.bttn_9 : edit.setText(edit.getText().toString()); break;
+            case R.id.btn_0 : edit.setText("0"); break;
+            case R.id.btn_1 : edit.setText("1"); break;
+            case R.id.btn_2 : edit.setText("2"); break;
+            case R.id.btn_3 : edit.setText("3"); break;
+            case R.id.btn_4 : edit.setText("4"); break;
+            case R.id.btn_5 : edit.setText("5"); break;
+            case R.id.btn_6 : edit.setText("6"); break;
+            case R.id.btn_7 : edit.setText("7"); break;
+            case R.id.btn_8 : edit.setText("8"); break;
+            case R.id.btn_9 : edit.setText("9"); break;
+
 
         }
-
-
-
-
     } // end onClick()
+
+
+
 
 
 } // end class
